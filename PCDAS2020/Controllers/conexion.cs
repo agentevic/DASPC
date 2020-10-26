191ab0166073ea6e2f57783cdc3d16caaee030ff
Víctor Ayala <43223855+agentevic@users.noreply.github.com>
2020-10-25 22:03:03 -0600

    Add files via upload

---
+++ PCDAS2020/Controllers/conexion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+
+namespace PCDAS2020.Controllers
+{
+    public class conexion
+    {
+        private string cadenaConexion { get; set; }
+        private SqlConnection conexionSQL;
+        public conexion()
+        {
+            cadenaConexion = @"Data source=(local); Initial Catalog=DASPC; Integrated Security =True";
+        }
+        public bool conectar()
+        {
+            try
+            {
+                this.conexionSQL = new SqlConnection(this.cadenaConexion);
+                this.conexionSQL.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public bool estadoconexion()
+        {
+            switch (this.conexionSQL.State)
+            {
+                case System.Data.ConnectionState.Broken:
+                    return true;
+                case System.Data.ConnectionState.Open:
+                    return true; 
+                default:
+                    return false;
+            }
+        }
+        public void desconectar()
+        {
+            this.conexionSQL.Close();
+        }
+    }
+}
---
+++ PCDAS2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PCDAS2020.MvcApplication" Language="C#" %>
